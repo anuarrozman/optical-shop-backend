e308668ad180c83bc6582d69d8709c5ad8c72c1e
anuarrozman <anuarrozman@heitech.com.my>
2026-02-03 00:47:59 +0800

    Initial commit: Spring Boot + PostgreSQL Neon setup

---
+++ src/main/java/com/optom/shop/OpticalShopApplication.java
@@ -0,0 +1,13 @@
+package com.optom.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticalShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optom/shop/OpticalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optom.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticalShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
